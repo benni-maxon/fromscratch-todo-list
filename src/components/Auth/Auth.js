94ae86e9c87174fa23ecf761b8a45cab7a0e55ea
benni maxon <benni.maxon@gmail.com>
2023-01-30 15:43:07 -0800

    panel box and input fields for Auth component

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { NavLink, Redirect, useParams } from 'react-router-dom';
 import { useUser } from '../../context/UserContext.js';
 
 export default function Auth() {
@@ -7,5 +7,43 @@ export default function Auth() {
   const [password, setPassword] = useState('');
   const { type } = useParams();
   const { user, setUser } = useUser();
-  return <div>Auth</div>;
+  return (
+    <div className="auth box">
+      <nav>
+        <div className="panel-heading">Welcome to your favorite to-do list!</div>
+        <div className="panel-tabs">
+          <NavLink to="/auth/sign-in" activeClassName="is-active">
+            Sign In
+          </NavLink>
+          <NavLink to="/auth/sign-up" activeClassName="is-active">
+            Sign Un
+          </NavLink>
+        </div>
+        <div className="panel-block">
+          <div className="field">
+            <label>Email</label>
+            <div>
+              <input
+                type="email"
+                placeholder="person@email.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label>Password</label>
+            <div>
+              <input
+                type="password"
+                placeholder="******"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 }
