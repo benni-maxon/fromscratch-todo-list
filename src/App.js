bfb2be80f28521133df4a0a01fa5e97849334c26
benni maxon <benni.maxon@gmail.com>
2023-01-31 13:01:21 -0800

    built Header component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 import Auth from './components/Auth/Auth.js';
 import Items from './components/Items/Items.js';
 import { useUser } from './context/UserContext.js';
+import Header from './components/Header/Header.js';
 
 function App() {
   const { user } = useUser();
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route path="/auth/:type" component={Auth} />
         <Route path="/items" component={Items} />
---
+++ src/components/Header/Header.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useUser } from '../../context/UserContext.js';
+import { signOut } from '../../services/auth.js';
+
+export default function Header() {
+  const { user, setUser } = useUser();
+
+  const handleLogout = async () => {
+    try {
+      await signOut();
+      setUser(null);
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
+  return (
+    <nav className="navbar" role="navigation">
+      <div>
+        <h1>✓To-Do List!✓</h1>
+        <div className="links">
+          {!user && (
+            <div className="sign-in-up-links">
+              <Link to="/auth/sign-up">Sign Up</Link>
+              <Link to="/auth/sign-in">Sign In</Link>
+            </div>
+          )}
+          {user && (
+            <>
+              <h2>hello {user.email}</h2>
+              <button onClick={handleLogout}>Sign Out</button>
+            </>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+}
