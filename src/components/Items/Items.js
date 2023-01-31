ce58cd3da0b1889c5b0e4869789c0deba9ce18fe
benni maxon <benni.maxon@gmail.com>
2023-01-30 17:58:03 -0800

    built Items component

--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { useUser } from '../../context/UserContext.js';
 
 export default function Items() {
-  return <div>Items</div>;
+  const { user } = useUser();
+  if (!user) {
+    return <Redirect to="/auth/sign-in" />;
+  }
+  return (
+    <div className="container">
+      <div>To-Do List</div>
+    </div>
+  );
 }
--- src/services/items.js
+++ src/services/items.js
@@ -1,13 +1,13 @@
 import { checkError, client } from './client.js';
 
 export async function getListItems() {
-  const response = await client.from('items').select();
+  const response = await client.from('todos').select();
 
   return checkError(response);
 }
 
 export async function createListItem(name) {
-  const response = await client.from('items').insert([{ name }]);
+  const response = await client.from('todos').insert([{ name }]);
 
   return checkError(response);
 }
